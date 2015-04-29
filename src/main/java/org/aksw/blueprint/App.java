16688cd1a78089457d911376e15ad79996e79ae7
Martin Röbert <roebert@infai.org>
2015-04-29 11:26:24 +0200

    Add pom and sample code

---
+++ src/main/java/org/aksw/blueprint/App.java
@@ -0,0 +1,21 @@
+/* 
+ * Copyright (C) 2015 AKSW
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package org.aksw.blueprint;
+
+/**
+ * Hello world!
+ * 
+ *  * @author Martin R&ouml;bert {$literal <roebert@infai.org>}
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
